815e91c4addfa6ca21bb4f3a13eac65d562b40d4
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-11-13 14:41:00 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Vatana Chhorn on 13/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
